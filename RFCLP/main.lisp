87b7ae370ed8fdcc505a3c0d03591553172cf07f
Israel Goncalves de Oliveira <prof.israel@gmail.com>
2018-09-27 10:39:17 -0300

    Funcionando com uma base real!

--- RFCLP/main.lisp
+++ RFCLP/main.lisp
@@ -1,9 +1,43 @@
 ;;;;;;;;;;;;;;;;;;;;
 (load "/root/quicklisp/setup.lisp")
-(load "random-sample.lisp")
+(load "load-sample.lisp")
+(load "tree.lisp")
 
-(format t "~S~%" (batch-samples (read)))
+(defun build-tree-limt (Ts)
+    (let (
+            (tree '())
+            (i 0)
+            )
+        (cl-csv:do-csv (row #P"adult.data")
+            (incf i 1)
+            (if (equal i 1)
+                (setf tree (create-tree (cons 'ROOT row)))
+                (grow-tree (cdr tree) (cons 'ROOT row))
+            )
+            (when (equal i Ts)
+                (return-from build-tree-limt tree)
+            )
+        )
+        tree
+    )
+)
 
-;(print (rand-lista (read) 50))
+(defun build-tree-full ()
+    (let (
+            (tree '())
+            (i 0)
+            )
+        (cl-csv:do-csv (row #P"adult.data_fix")
+            (incf i 1)
+            (if (equal i 1)
+                (setf tree (create-tree (cons 'ROOT row)))
+                (grow-tree (cdr tree) (cons 'ROOT row))
+            )
+        )
+        tree
+    )
+)
 
-
+;(defvar tree (build-tree-limt (read)))
+(defvar tree (build-tree-full))
+(show-tree tree)
